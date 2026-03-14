2d6c4b642249287e839fb11fc36a1bfd166086c8
toximu <timofei625211@gmail.com>
2026-03-14 13:01:30 +0300

    queue: solution
    # Conflicts:
    #	src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java

--- src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
+++ src/main/java/hse/java/lectures/lecture6/tasks/queue/BoundedBlockingQueue.java
@@ -1,18 +1,46 @@
 package hse.java.lectures.lecture6.tasks.queue;
 
+import java.util.Queue;
+
 public class BoundedBlockingQueue<T> {
 
-
+    Queue<T> queue;
+    private int capacity;
+    private int size = 0;
+    Object monitor;
     public BoundedBlockingQueue(int capacity) {
-
+        this.capacity = capacity;
     }
 
-    public void put(T item) throws InterruptedException {
+    public void put(T item) {
+        synchronized (monitor) {
+            if (size == capacity) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
 
+            queue.add(item);
+            size++;
+            monitor.notifyAll();
+        }
     }
 
-    public T take() throws InterruptedException {
-        return null;
+    public T take() {
+        synchronized (monitor) {
+            if (size == 0) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            size--;
+            monitor.notifyAll();
+            return queue.poll();
+        }
     }
 
     public int size() {
