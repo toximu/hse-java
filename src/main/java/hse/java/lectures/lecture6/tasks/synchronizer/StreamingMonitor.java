4fb9c5259c2b4da3ea266363715998bc88d69b14
toximu <timofei625211@gmail.com>
2026-03-20 18:20:44 +0300

    synchronizer: solution

--- src/main/java/hse/java/lectures/lecture6/tasks/synchronizer/StreamingMonitor.java
+++ src/main/java/hse/java/lectures/lecture6/tasks/synchronizer/StreamingMonitor.java
@@ -1,5 +1,34 @@
 package hse.java.lectures.lecture6.tasks.synchronizer;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class StreamingMonitor {
-    // impl your sync here
+    int shouldPrintNow = 0;
+    List<Integer> ids;
+    boolean isWorking = true;
+    int ticksDone = 0;
+    int tickNeeded = 0;
+    StreamingMonitor(List<Integer> ids, int tickNeeded) {
+        this.ids = new ArrayList<>(ids);
+        this.ids.sort(Comparator.naturalOrder());
+        this.tickNeeded = tickNeeded;
+    }
+
+    Integer nowWriter() {
+        return ids.get(shouldPrintNow);
+    }
+
+    void next() {
+        shouldPrintNow++;
+        if (shouldPrintNow == ids.size()) {
+            shouldPrintNow = 0;
+            ticksDone++;
+        }
+
+        if (ticksDone == tickNeeded) {
+            isWorking = false;
+        }
+    }
 }
